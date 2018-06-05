084a367883cf4af39f8f4a5c2daa49a77cafc381
Keith Frederick <elek01@gmail.com>
2018-06-05 19:56:22 -0400

    This is the changes to ind.ex html, and minor changes to circleman.

--- JS/CircleMan.js
+++ JS/CircleMan.js
@@ -1,87 +1,9 @@
-// var myGamePiece;
-
-// function startGame() {
-//     myGamePiece = new component(30, 30, "red", 10, 120);
-//     myGameArea.start();
-// }
-
-// var myGameArea = {
-//     canvas : document.createElement("canvas"),
-//     start : function() {
-//         this.canvas.width = 480;
-//         this.canvas.height = 270;
-//         this.context = this.canvas.getContext("2d");
-//         document.body.insertBefore(this.canvas, document.body.childNodes[6]);
-//         this.interval = setInterval(updateGameArea, 20);        
-//     },
-//     stop : function() {
-//         clearInterval(this.interval);
-//     },    
-//     clear : function() {
-//         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-//     }
-// }
-
-// function component(width, height, color, x, y) {
-//     this.type = type;
-//     this.width = width;
-//     this.height = height;
-//     this.x = x;
-//     this.y = y;    
-//     // this.speedX = 0;
-//     // this.speedY = 0;    
-//     // this.gravity = 0.1;
-//     // this.gravitySpeed = 0;
-//     // this.bounce = 0.6;    
-//     // Sets the bounce of the walls
-//     // this.bounce = 1;
-//     this.update = function() {
-//         ctx = myGameArea.context;
-//         ctx.fillStyle = color;
-//         ctx.fillRect(this.x, this.y, this.width, this.height);
-//     }
-//     this.newPos = function() {
-//         this.x += this.speedX;
-//         this.y += this.speedY;        
-//     }    
-
-
-//     // this.hitBottom = function() {
-//     //     var rockbottom = myGameArea.canvas.height - this.height;
-//     //     if (this.y > rockbottom) {
-//     //         this.y = rockbottom;
-//     //         this.gravitySpeed = -(this.gravitySpeed * this.bounce);
-//     //     }
-//     // }
-
-// }
-// function updateGameArea() {
-//     myGameArea.clear();
-//     myGamePiece.newPos();    
-//     myGamePiece.update();
-// }
-
-// function moveup() {
-//     myGamePiece.speedY -= 1; 
-// }
-
-// function movedown() {
-//     myGamePiece.speedY += 1; 
-// }
-
-// function moveleft() {
-//     myGamePiece.speedX -= 1; 
-// }
-
-// function moveright() {
-//     myGamePiece.speedX += 1; 
-// }
-    
-
 var myGamePiece;
+var myScore;
 
 function startGame() {
     myGamePiece = new component(30, 30, "red", 80, 75);
+    myScore = new component("30px", "Consolas", "black", 40, 40, "text");
     myGameArea.start();
 }
 
@@ -91,7 +13,7 @@ var myGameArea = {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[6]);
+        document.body.insertBefore(this.canvas, document.body.childNodes[3]);
         this.interval = setInterval(updateGameArea, 20);        
     },
     stop : function() {
@@ -115,8 +37,11 @@ function component(width, height, color, x, y, type) {
     this.bounce = 0.3;
     this.update = function() {
         ctx = myGameArea.context;
-
-        
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
         ctx.beginPath();
         ctx.arc(this.x,this.y,30,0,2*Math.PI);
         ctx.fillStyle = color;
@@ -126,7 +51,7 @@ function component(width, height, color, x, y, type) {
         // ctx.lineWidth = 30;
         // ctx.strokeStyle = "#000000";
         ctx.stroke();
-
+          }
 
         // ctx.beginPath();
         // ctx.arc(this.x,this.y,100,0,2*Math.PI);
